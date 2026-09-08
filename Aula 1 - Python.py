846ed710ca6e5a452498d244d4daf13edafaf5fd
Humberto Elias Garcia Lopes <quadriviumacademic@gmail.com>
2026-09-08 15:22:00 -0300

    Pequenos ajustes feitos no texto do Jupyter

---
+++ Aula 1 - Python.py
@@ -0,0 +1,35 @@
+print(3 + 4)
+print( 123.12 + 54.78)
+print(7.33 + 9.00)
+
+print(123.45 - 45.66)
+print(4 - 12.33)
+print(1078.65 - 13.66)
+
+print(3 * 12)
+print(43.87 * 45.11)
+print(4 * 8.33)
+
+print(3.04 / 1.5)
+print(3.04 // 1.5)
+print(3.04 % 1.5)
+
+print(12.33 ** 6.77)
+print(4.52 ** 12.98)
+print(7.33 ** 1.03)
+
+print(5 ** (1/2))
+print(18.33 ** (1/3))
+print(134.55 ** (1/11))
+
+import math
+print(math.log(3.45, 2))
+print(math.log10(3.45))
+print(math.log(3.45))
+
+x = (4 + 5)
+print(x)
+y = (12.45 - 1.07)
+print(y)
+z = x ** y
+print(z)
